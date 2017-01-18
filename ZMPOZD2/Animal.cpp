12def08e9a94da063e6906ba22ec81211b42889f
Witold <w.mormul@gmail.com>
2017-01-18 13:27:50 +0100

    class Animal

---
+++ ZMPOZD2/Animal.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "Animal.h"
+#include <iostream>
+
+using namespace std;
+
+Animal::Animal()
+{
+	cout << "Konstruktor Animal." << endl;
+}
+
+
+Animal::~Animal()
+{
+	cout << "Destruktor Animal." << endl;
+}
--- ZMPOZD2/ZMPOZD2.cpp
+++ ZMPOZD2/ZMPOZD2.cpp
@@ -2,10 +2,13 @@
 //
 
 #include "stdafx.h"
+#include "Animal.h"
 
+using namespace std;
 
 int main()
 {
+	Animal animal;
     return 0;
 }
 
